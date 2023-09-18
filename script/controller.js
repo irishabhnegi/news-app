8cf7cccdf40a23ca850259d51c427118d70f6077
irishabhnegi <rishabhnegi175@gmail.com>
2023-09-18 11:55:38 +0530

    build up top head line new scroll media section

--- script/controller.js
+++ script/controller.js
@@ -1,36 +1,36 @@
 const parentEl = document.querySelector('#media-scroller')
-async function get() {
-  let data = await fetch(
-    ' https://newsapi.org/v2/everything?q=tesla&from=2023-08-17&sortBy=publishedAt&apiKey=fa2f06f253ed42849ef056fbb6f95446'
+
+const createTopHeading = async () => {
+  const response = await fetch(
+    'https://newsapi.org/v2/top-headlines?country=in&category=business&apiKey=fa2f06f253ed42849ef056fbb6f95446'
   )
 
-  let pure = await data.json()
-  let dataMain = pure.articles[1]
+  const { articles } = await response.json()
+  console.log(articles)
 
-  let state = {
-    author: dataMain.author,
-    title: dataMain.title,
-    image: dataMain.urlToImage,
-  }
-
-  console.log(pure.articles[1])
-  console.log(state)
-
-  let markup = `
+  const markup = articles
+    .map((article) => {
+      return `
       <div class="scroller-news-container">
-                <div class="news-img-info">
-                        <img src=${state.image} alt="news">
-                        <div class="new-info">
-                            <h2 class="author">${state.author}</h2>
-                            <h2 class="date-of-publish">12-03-12</h2>
-                        </div>
-                    </div>
-
-
-                    <p>${state.title}</p>
-                </div> `
-
+              <div class="news-img-info">
+              <div class="new-info">
+              <h2 class="author">${article.author}</h2>
+              <h2 class="date-of-publish">${article.publishedAt.slice(
+                0,
+                10
+              )}</h2>
+              </div>
+              <a href=${article.url}>
+                <img src=${article.urlToImage} alt="news">
+              </a>
+              </div>
+              <p>${article.title}</p>
+        </div>
+    `
+    })
+    .join('')
+  parentEl.innerHTML = ''
   parentEl.insertAdjacentHTML('afterbegin', markup)
 }
 
-get()
+createTopHeading()
